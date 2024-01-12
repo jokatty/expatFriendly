ee31ae8b696af35688d1bc09aa24cfb57eff81f3
jokatty <jyotikattani@gmail.com>
2024-01-12 14:55:53 +0100

    add user model

---
+++ db/models/user.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {}
+  User.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.INTEGER,
+      },
+      password: {
+        type: DataTypes.INTEGER,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: new Date(),
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: new Date(),
+      },
+    },
+    {
+      sequelize,
+      modelName: "user",
+      underscored: true,
+    }
+  );
+  return Product;
+};
