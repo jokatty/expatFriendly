13134e1efa5075276021c3a7c00729ddafc981a3
jokatty <jyotikattani@gmail.com>
2024-01-16 13:50:56 +0100

    add questions route

---
+++ Routers/QuestionsRoute.js
@@ -0,0 +1,17 @@
+class QuestionsRouter {
+  constructor(express, controller) {
+    this.express = express;
+    this.controller = controller;
+  }
+
+  routes() {
+    const router = this.express.Router();
+
+    router.get("/", this.controller.getAll.bind(this.controller));
+    router.get("/:questionId", this.controller.getOne.bind(this.controller));
+    router.post("/", this.controller.insertOne.bind(this.controller));
+    return router;
+  }
+}
+
+module.exports = QuestionsRouter;
