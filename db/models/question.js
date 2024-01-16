4e5212822a39d8bb748002a255fd2581f64b01b0
jokatty <jyotikattani@gmail.com>
2024-01-16 13:18:49 +0100

    add question model

---
+++ db/models/question.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {}
+  Question.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      question: {
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: new Date(),
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: new Date(),
+      },
+    },
+    {
+      sequelize,
+      modelName: "question",
+      underscored: true,
+    }
+  );
+  return Question;
+};
