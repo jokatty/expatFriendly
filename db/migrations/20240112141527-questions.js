53a494fdc208f46366aa15886226a6f9decc7696
jokatty <jyotikattani@gmail.com>
2024-01-16 13:20:11 +0100

    add questions migration

---
+++ db/migrations/20240112141527-questions.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("questions", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      question: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("questions");
+  },
+};
