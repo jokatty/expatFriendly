a482ee590dc47bffc8a60aecd80bd9925aa89e63
jokatty <jyotikattani@gmail.com>
2024-01-23 13:51:44 +0100

    add logic to retrieve one question

--- controllers/questionsController.mjs
+++ controllers/questionsController.mjs
@@ -11,8 +11,27 @@ export default function initQuestionController(db){
       });
     }
   }
+
+  // Get an specific question
+  const getOneQuestion = async(req,res) =>{
+      try {
+      const question = await db.Question.findOne({
+        where: {
+          id: req.params.id
+        }
+      });
+      res.status(200).json(question);
+    } catch (error) {
+      res.status(500).json({
+        message: "Error retrieving one question",
+        error: error.message,
+      });
+    }
+  }
+
   return {
     allQuestions,
+    getOneQuestion
   }
 
 }
